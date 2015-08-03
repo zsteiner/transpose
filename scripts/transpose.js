0f65262ef44ed12c0c4ac96e0c055b6db3df9a08
Zach Steiner <zsteiner@gmail.com>
2015-08-03 10:43:00 -0400

    Style Tweaks

--- scripts/transpose.js
+++ scripts/transpose.js
@@ -9,11 +9,11 @@ var originalNote,
     transposeFactor;
 
 var noteTranslator = ['C','G','D','A','E','B',
-    'F<span class="flat-sharp">&#X266F;</span> / G<span class="flat-sharp">&#X266D;</span>',
-    'D<span class="flat-sharp">&#X266D;</span> / C<span class="flat-sharp">&#X266F;</span>',
-    'A<span class="flat-sharp">&#X266D;</span> / G<span class="flat-sharp">&#X266F;</span>',
-    'E<span class="flat-sharp">&#X266D;</span> / D<span class="flat-sharp">&#X266F;</span>',
-    'B<span class="flat-sharp">&#X266D;</span> / A<span class="flat-sharp">&#X266F;</span>',
+    'F<span class="sharp">&#X266F;</span> / G<span class="flat">&#X266D;</span>',
+    'D<span class="flat">&#X266D;</span> / C<span class="sharp">&#X266F;</span>',
+    'A<span class="flat">&#X266D;</span> / G<span class="sharp">&#X266F;</span>',
+    'E<span class="flat">&#X266D;</span> / D<span class="sharp">&#X266F;</span>',
+    'B<span class="flat">&#X266D;</span> / A<span class="sharp">&#X266F;</span>',
     'F'
 ];
 
