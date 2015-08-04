61d91c6465099949e895ae6952d3771f023bda9f
Zach Steiner <zsteiner@gmail.com>
2015-08-04 11:25:22 -0400

    Big Refactor
    Added grid instrument selection. Refactored JS to work with new input.
    Better error handling. Better mobile experience.

---
+++ scripts/instruments.js
@@ -0,0 +1,162 @@
+[
+  {
+    "name": "piano",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": true,
+    "iconName": "piano",
+    "default": true
+  },
+  {
+    "name": "alto clarinet",
+    "key": "e-flat",
+    "transposeFactor": 3,
+    "icon": false,
+    "iconName": "e-flat",
+    "default": false
+  },
+  {
+    "name": "alto sax",
+    "key": "e-flat",
+    "transposeFactor": 3,
+    "icon": true,
+    "iconName": "alto-sax",
+    "default": false
+  },
+  {
+    "name": "baritone sax",
+    "key": "e-flat",
+    "transposeFactor": 3,
+    "icon": false,
+    "iconName": "e-flat",
+    "default": false
+  },
+  {
+    "name": "baritone horn",
+    "key": "b-flat",
+    "transposeFactor": 2,
+    "icon": false,
+    "iconName": "b-flat",
+    "default": false
+  },
+  {
+    "name": "bass clarinet",
+    "key": "b-flat",
+    "transposeFactor": 2,
+    "icon": false,
+    "iconName": "b-flat",
+    "default": false
+  },
+  {
+    "name": "cello",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  },
+  {
+    "name": "clarinet",
+    "key": "b-flat",
+    "transposeFactor": 2,
+    "icon": false,
+    "iconName": "b-flat",
+    "default": false
+  },
+  {
+    "name": "double bass",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  },
+  {
+    "name": "flute",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  },
+  {
+    "name": "guitar",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  },
+  {
+    "name": "french horn",
+    "key": "f",
+    "transposeFactor": 1,
+    "icon": false,
+    "iconName": "f",
+    "default": false
+  },
+  {
+    "name": "oboe",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  },
+  {
+    "name": "soprano sax",
+    "key": "b-flat",
+    "transposeFactor": 2,
+    "icon": false,
+    "iconName": "b-flat",
+    "default": false
+  },
+  {
+    "name": "tenor sax",
+    "key": "b-flat",
+    "transposeFactor": 2,
+    "icon": false,
+    "iconName": "b-flat",
+    "default": false
+  },
+  {
+    "name": "trombone",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  },
+  {
+    "name": "trumpet",
+    "key": "b-flat",
+    "transposeFactor": 2,
+    "icon": false,
+    "iconName": "b-flat",
+    "default": false
+  },
+  {
+    "name": "tuba",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  },
+  {
+    "name": "viola",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  },
+  {
+    "name": "violin",
+    "key": "c",
+    "transposeFactor": 0,
+    "icon": false,
+    "iconName": "c",
+    "default": false
+  }
+]
--- scripts/transpose.js
+++ scripts/transpose.js
@@ -1,7 +1,9 @@
-var originalNote,
+var instruments,
+    instrumentScroll,
+    originalNote,
     originalNoteLabel,
-    originalInstrument,
-    originalInstrumentLabel,
+    originalInstrument = 0,
+    originalInstrumentLabel = "piano",
     transposeInstrument,
     transposeInstrumentLabel,
     transposeNote,
@@ -14,97 +16,202 @@ var noteTranslator = ['C','G','D','A','E','B',
     'A<span class="flat">&#X266D;</span> / G<span class="sharp">&#X266F;</span>',
     'E<span class="flat">&#X266D;</span> / D<span class="sharp">&#X266F;</span>',
     'B<span class="flat">&#X266D;</span> / A<span class="sharp">&#X266F;</span>',
-    'F'
-];
+    'F'];
+
+function mainScroll() {
+	instrumentScroll = new IScroll('#scroll1', {
+		scrollbars: false,
+		mouseWheel: true,
+		interactiveScrollbars: true,
+		shrinkScrollbars: 'scale',
+		click: true,
+		fadeScrollbars: false,
+		preventDefaultException: { tagName: /^(INPUT|TEXTAREA|BUTTON|SELECT|LABEL|A)$/ }
+	});
+}
 
 function noteTranslation() {
-	--transposeNote;	
-	transposeNoteLabel = noteTranslator[transposeNote];
+	transposeNoteIndex = transposeNote - 1;	
+	transposeNoteLabel = noteTranslator[transposeNoteIndex];
 
-	--originalNote;	
-	originalNoteLabel = noteTranslator[originalNote];
+	originalNoteIndex = originalNote - 1;	
+	originalNoteLabel = noteTranslator[originalNoteIndex];
 }
 
 function transpose() {
-    originalInstrument = $('#instrument-list1').val();
-    transposeInstrument = $('#instrument-list2').val();
-    originalInstrumentLabel = $('#instrument-list1 option:selected').text();
-    transposeInstrumentLabel = $('#instrument-list2 option:selected').text();
+    transposeFactor = transposeInstrument - originalInstrument;        
 
-        originalNote = parseInt(originalNote, 10);
-        originalInstrument = parseInt(originalInstrument, 10);
-        transposeInstrument = parseInt(transposeInstrument, 10);
-        
-        transposeFactor = transposeInstrument - originalInstrument;        
+    //console.log('Original Note: ' + originalNote);
+    //console.log(originalInstrumentLabel + ' (' + originalInstrument  + ') -> ' + transposeInstrumentLabel + '(' + transposeInstrument + ') | ' + 'transpose factor: ' + transposeFactor);
+
+    if ((originalNote + transposeFactor) <= 0) {
+        transposeNote = (originalNote + transposeFactor) + 12;
+    }
+
+    else if ((originalNote + transposeFactor) > 12) {
+        transposeNote = (originalNote + transposeFactor) - 12;
+    }
+
+    else {
+        transposeNote = originalNote + transposeFactor;
+    }
     
-        console.log(originalInstrumentLabel + ' -> ' + transposeInstrumentLabel + ', ' + 'transpose factor: ' + transposeFactor);
+    //console.log('Transposed Note: ' + transposeNote);
+     
+    noteTranslation();
+
+    $('#item-' + transposeNote).addClass('is-transposed').siblings().removeClass('is-transposed');
     
-        if ((originalNote + transposeFactor) <= 0) {
-            transposeNote = (originalNote + transposeFactor) + 12;
-        }
+    if (transposeNote === originalNote) {
+        $('.answer-same').removeClass('is-hidden').siblings().addClass('is-hidden');
+        $('.original-instrument').html(originalInstrumentLabel);
+        $('.transpose-instrument').html(transposeInstrumentLabel);
+    }
     
-        else if ((originalNote + transposeFactor) > 12) {
-            transposeNote = (originalNote + transposeFactor) - 12;
-        }
-    
-        else {
-            transposeNote = originalNote + transposeFactor;
-        }
-        
-        console.log('Original Note: ' + originalNote);
-        console.log('Transposed Note: ' + transposeNote);
-         
-        noteTranslation();
-    
-        $('#item-' + (transposeNote + 1)).addClass('is-transposed').siblings().removeClass('is-transposed');
-        
-        if (transposeNote === originalNote) {
-           $('.answer-same').removeClass('is-hidden').siblings().addClass('is-hidden');
-        	$('.original-note').html(originalNoteLabel);
-        	$('.transpose-note').html(transposeNoteLabel);
-        }
-        
-        else {
-            $('.answer').removeClass('is-hidden').siblings().addClass('is-hidden');
-        	$('.original-instrument').html(originalInstrumentLabel);
-        	$('.original-note').html(originalNoteLabel);
-        	$('.transpose-note').html(transposeNoteLabel);
-        	$('.transpose-instrument').html(transposeInstrumentLabel);
-        }
-        
+    else {
+        $('.answer').removeClass('is-hidden').siblings().addClass('is-hidden');
+    	$('.original-instrument').html(originalInstrumentLabel);
+    	$('.original-note').html(originalNoteLabel);
+    	$('.transpose-note').html(transposeNoteLabel);
+    	$('.transpose-instrument').html(transposeInstrumentLabel);
+    }
 }
 
 function errorChecker() {
     if (originalNote === undefined) {
-        console.log('Error');
-        $('.error-message').removeClass('is-hidden');  
+        $('.error-message').removeClass('is-hidden').siblings().addClass('is-hidden');  
+    }
+    
+    else if (transposeInstrument === undefined) {
+        $('.error-message-transpose').removeClass('is-hidden').siblings().addClass('is-hidden');
     }
 
     else {
+        originalNote = $('#itemsContainer a.is-selected').attr('id');
         originalNote =  originalNote.substring(5);
-        
+        originalNote = parseInt(originalNote, 10);
+
         transpose();    
     }
 }
 
-$('#instrument-list1').change(function(){
-    originalNote = $('.item.is-selected').attr('id');
-    originalInstrument = $(this).val();
-
-    errorChecker();    
-});
-
-$('#instrument-list2').change(function(){
-    originalNote = $('.item.is-selected').attr('id');
-    transposeInstrument = $(this).val();
+function createInstruments(obj) {
+    var icon;
+    var sourceInstrumentList = document.getElementById('source-list');
+    var transposeInstrumentList = document.getElementById('transpose-list');
     
-    errorChecker();
+    $.each(obj, function(i, sourceItem, transposeItem) {
+        sourceItem = sourceInstrumentList.appendChild(document.createElement('li'));
+        transposeItem = transposeInstrumentList.appendChild(document.createElement('li'));
+
+        if (instruments[i].icon === true) {
+            icon = '<svg class="icon"><use xlink:href="#icon-' + instruments[i].iconName + '"></use></svg>';    
+        }
+        
+        else {
+            icon = '<svg class="icon icon-note"><use xlink:href="#icon-' + instruments[i].key + '"></use></svg>';
+        }
+        sourceItem.innerHTML = 
+    	    '<input type="radio" name="source-instrument" id="source-instrument' + i + '" value="' + instruments[i].transposeFactor + '">' + 
+    	    '<label for="source-instrument' + i + '">' + icon + 
+    	    '<span class="instrument-name">' + instruments[i].name + '</span></label>';
+
+        transposeItem.innerHTML = 
+    	    '<input type="radio" name="transpose-instrument" id="transpose-instrument' + i + '" value="' + instruments[i].transposeFactor + '" >' +
+    	    '<label for="transpose-instrument' + i + '">' + icon + 
+    	    '<span class="instrument-name">' + instruments[i].name + '</span></label>';
+    	    
+        $(sourceItem).find('label').on('click', function(event) {
+            event.stopPropagation();
+
+            selectedInstrument = $(this).attr('for');
+            selectedInstrument = selectedInstrument.replace('source-instrument','');
+            
+            originalInstrument =  instruments[selectedInstrument].transposeFactor;
+            originalInstrumentLabel = instruments[selectedInstrument].name;
+            
+            $('.instrument-source .instrument-name').text(originalInstrumentLabel);
+
+            if (instruments[i].icon === true) {
+                $('.instrument-source use').attr('xlink:href', '#icon-' + instruments[selectedInstrument].iconName).parent().removeClass('icon-note');
+            }
+            
+            else {
+                $('.instrument-source use').attr('xlink:href', '#icon-' + instruments[selectedInstrument].iconName).parent().addClass('icon-note');
+            }
+            
+            setTimeout(function(){
+                $('.instrument-selector').addClass('is-hidden');    
+            },200);
+            
+            $('.instrument-source .button').removeClass('is-selected button-empty');
+            
+            errorChecker();
+        });
+
+        $(transposeItem).find('label').on('click', function(event) {
+            event.stopPropagation();
+            
+            selectedInstrument = $(this).attr('for');
+            selectedInstrument = selectedInstrument.replace('transpose-instrument','');
+
+            transposeInstrument = instruments[selectedInstrument].transposeFactor;
+            transposeInstrumentLabel = instruments[selectedInstrument].name;
+
+            $('.instrument-transpose .instrument-name').text(transposeInstrumentLabel);
+            
+            if (instruments[i].icon === true) {
+                $('.instrument-transpose use').attr('xlink:href', '#icon-' + instruments[selectedInstrument].iconName).parent().removeClass('icon-note');
+            }
+            
+            else {
+                $('.instrument-transpose use').attr('xlink:href', '#icon-' + instruments[selectedInstrument].iconName).parent().addClass('icon-note');
+            }
+            
+            setTimeout(function(){
+                $('.instrument-selector').addClass('is-hidden');    
+            },200);
+
+            $('.instrument-transpose .button').removeClass('is-selected button-empty');
+            event.stopPropagation();
+            errorChecker();
+        });
+    });   
+    instrumentScroll.refresh();
+}
+
+mainScroll();
+
+$.getJSON('scripts/instruments.js').done(function(data) {
+    instruments = data;
+    createInstruments(instruments);
 });
 
-$('#itemsContainer a').click(function(){
+$('.instrument .button').click(function(event){
+    event.stopPropagation();
+    var value = $(this).data('value');
+
+    $('.instrument .button').removeClass('is-selected'); 
+    $(this).addClass('is-selected');
+    
+    $('.instrument-selector').removeClass('is-hidden');
+    $('#' + value + '-list').removeClass('is-hidden').siblings().addClass('is-hidden');
+    
+    instrumentScroll.refresh();
+});
+
+$('body').click(function(){
+     $('.instrument-selector').addClass('is-hidden');
+     $('.button').removeClass('is-selected');
+});
+
+$('#itemsContainer a').click(function(event){
+    event.stopPropagation();
+
     originalNote = $(this).attr('id');
     originalNote =  originalNote.substring(5);
-    
+    originalNote = parseInt(originalNote, 10);
+
     $(this).addClass('is-selected').siblings().removeClass('is-selected');
-    transpose();
+    errorChecker();
 });
