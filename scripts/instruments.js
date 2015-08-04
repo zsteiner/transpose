c7011fb69aa5332c6b7aab91789e14ef91b8cf04
Zach Steiner <zsteiner@gmail.com>
2015-08-04 15:41:23 -0400

    Add more icons.

--- scripts/instruments.js
+++ scripts/instruments.js
@@ -5,7 +5,7 @@
     "transposeFactor": 0,
     "icon": true,
     "iconName": "piano",
-    "default": true
+    "default": true 
   },
   {
     "name": "alto clarinet",
@@ -35,8 +35,8 @@
     "name": "baritone horn",
     "key": "b-flat",
     "transposeFactor": 2,
-    "icon": false,
-    "iconName": "b-flat",
+    "icon": true,
+    "iconName": "baritone-horn",
     "default": false
   },
   {
@@ -51,112 +51,112 @@
     "name": "cello",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "cello",
     "default": false
   },
   {
     "name": "clarinet",
     "key": "b-flat",
     "transposeFactor": 2,
-    "icon": false,
-    "iconName": "b-flat",
+    "icon": true,
+    "iconName": "clarinet",
     "default": false
   },
   {
     "name": "double bass",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "double-bass",
     "default": false
   },
   {
     "name": "flute",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "flute",
     "default": false
   },
   {
     "name": "guitar",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "guitar",
     "default": false
   },
   {
     "name": "french horn",
     "key": "f",
     "transposeFactor": 1,
-    "icon": false,
-    "iconName": "f",
+    "icon": true,
+    "iconName": "french-horn",
     "default": false
   },
   {
     "name": "oboe",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "oboe",
     "default": false
   },
   {
     "name": "soprano sax",
     "key": "b-flat",
     "transposeFactor": 2,
-    "icon": false,
-    "iconName": "b-flat",
+    "icon": true,
+    "iconName": "soprano-sax",
     "default": false
   },
   {
     "name": "tenor sax",
     "key": "b-flat",
     "transposeFactor": 2,
-    "icon": false,
-    "iconName": "b-flat",
+    "icon": true,
+    "iconName": "tenor-sax",
     "default": false
   },
   {
     "name": "trombone",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "trombone",
     "default": false
   },
   {
     "name": "trumpet",
     "key": "b-flat",
     "transposeFactor": 2,
-    "icon": false,
-    "iconName": "b-flat",
+    "icon": true,
+    "iconName": "trumpet",
     "default": false
   },
   {
     "name": "tuba",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "tuba",
     "default": false
   },
   {
     "name": "viola",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "violin",
     "default": false
   },
   {
     "name": "violin",
     "key": "c",
     "transposeFactor": 0,
-    "icon": false,
-    "iconName": "c",
+    "icon": true,
+    "iconName": "violin",
     "default": false
   }
 ]
