25f5ac83de3218479b819484d232677ec01eb8ab
Zach Steiner <zsteiner@gmail.com>
2020-03-29 18:16:56 -0600

    Add message

---
+++ src/components/TransposeMessage.vue
@@ -0,0 +1,31 @@
+<template>
+  <p class="message">
+    <span v-if="originalNote && transposedNote">
+      <strong v-html="originalNote"></strong> on the
+      {{ instrument1.name }} sounds the same as
+      <strong v-html="transposedNote"></strong> on the {{ instrument2.name }}.
+    </span>
+  </p>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  name: 'TransposeMessage',
+  props: {
+    originalNote: String,
+    transposedNote: String,
+  },
+  computed: {
+    ...mapState(['instrument1', 'instrument2']),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.message {
+  font-size: 1.25rem;
+  min-height: 4rem;
+  margin-bottom: 3rem;
+}
+</style>
