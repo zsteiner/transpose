056d7ccaa28834600350832303697b27591355e9
Zach Steiner <zsteiner@gmail.com>
2020-03-29 19:19:16 -0600

    Remove console log

--- src/components/Picker.vue
+++ src/components/Picker.vue
@@ -39,7 +39,6 @@ export default {
       this.$emit('click');
     },
     selectInstrument(instrument) {
-      console.log('instrument', instrument);
       const { selection } = this;
       this.updateSelection({
         selection,
