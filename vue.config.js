8fde78ff4034dfc1b0ec439cd6705057e762e4d2
Zach Steiner <zsteiner@gmail.com>
2022-03-27 11:48:47 -0600

    Fix stylelint config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -11,5 +13,16 @@ module.exports = {
     if (process.env.NODE_ENV === 'development') {
       config.output.filename('[name].[hash].js').end();
     }
+  },
+
+  configureWebpack: (config) => {
+    config.plugins.push(
+      new StyleLintPlugin({
+        files: ['src/**/*.{vue,css,scss}'],
+        failOnError: false,
+        cache: true,
+        fix: true,
+      }),
+    );
   },
 };
