6e6c09691e079c1bb8903bc8367c41d9c3218de1
Zach Steiner <zsteiner@gmail.com>
2024-08-25 14:44:55 -0600

    layotu

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,12 +2,16 @@ import { MainNav } from '@/components/MainNav';
 import { TransposeNote } from '@/components/TransposeNote';
 import { Container } from '@/types';
 
+import styles from './page.module.css';
+
 const MainAppPage = ({ children }: Container) => {
   return (
     <>
       <MainNav />
       <TransposeNote>
-        {children}
+        <div className={styles.content}>
+          {children}
+        </div>
       </TransposeNote>
     </>
   );
