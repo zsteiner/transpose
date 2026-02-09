1b171c5d0f21d8fc86628f387da2655a0b87600d
Zach Steiner <zsteiner@gmail.com>
2026-02-09 08:46:29 -0700

    Refactor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,5 @@
 import nextConfig from 'eslint-config-next';
 
-// Polyfill for structuredClone if not available
-if (typeof structuredClone === 'undefined') {
-  globalThis.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
-}
-
 const eslintConfig = [
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**'],
